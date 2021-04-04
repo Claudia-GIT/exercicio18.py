a2f2d5e912fe6a9fe2f7ab3544e120425cec141a
Claudia <santos_claudi@yahoo.com.br>
2021-04-03 22:58:56 -0300

    Adicionar arquivo sem resolucão

---
+++ exercicio18.py
@@ -0,0 +1,4 @@
+#DESAFIO 18 - um sistema que recebe três notas de um aluno e define se ele passou ou não de ano.
+#Nota mínima de aprovação: 7
+#Entre 5 e 6.9, o aluno ganha o direito de fazer uma recuperação.
+#Caso contrário, o aluno será reprovado.
